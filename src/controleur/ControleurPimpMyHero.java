4cdebfd0c82d1dcaef2e5941005dfa0cdde419d1
Noah Nicolas <53911681+Witop-s@users.noreply.github.com>
2023-09-26 11:41:31 -0400

    Blueprint handler

--- src/controleur/ControleurPimpMyHero.java
+++ src/controleur/ControleurPimpMyHero.java
@@ -11,4 +11,7 @@ public class ControleurPimpMyHero extends Controleur {
 
     public void initialiser() {
     }
+
+    public void notifierSelectionCheveux() {
+    }
 }
--- src/vue/VuePimpMyHero.java
+++ src/vue/VuePimpMyHero.java
@@ -3,6 +3,9 @@ package vue;
 import architecture.Vue;
 import com.sun.media.jfxmedia.logging.Logger;
 import controleur.ControleurPimpMyHero;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 
 public class VuePimpMyHero extends Vue {
 
@@ -24,6 +27,19 @@ public class VuePimpMyHero extends Vue {
 
     public void activerControles() {
         super.activerControles();
-    }
 
+        /*
+        Button selectionnerCheveux = (Button) lookup("#selectionner-cheveux");
+        selectionnerCheveux.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent actionEvent)
+            {
+                Logger.logMsg(Logger.INFO, "Bouton Choisir Cheveux activé");
+                controleur.notifierSelectionCheveux();
+            }
+        });
+
+         */
+    }
 }
