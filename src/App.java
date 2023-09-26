4334735586c29f3a2e8dfed49432f439451e0aa3
Noah Nicolas <53911681+Witop-s@users.noreply.github.com>
2023-09-26 10:36:23 -0400

    Addition main

---
+++ src/App.java
@@ -0,0 +1,10 @@
+//import architecture.Controleur;
+//import architecture.Fenetre;
+
+public class App {
+    public static void main(String[] parametres) {
+        System.out.println("Hello Editeur!");
+        //Controleur.choisirVuePrincipale(VueJardinator.class);
+        //Fenetre.launch(Fenetre.class, parametres);
+    }
+}
